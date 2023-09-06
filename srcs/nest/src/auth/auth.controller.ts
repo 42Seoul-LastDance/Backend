6131eb8e44baaeef318b5dccef0f4020c1cd86e5
HyunJun KIM <hihj070914@icloud.com>
2023-09-06 21:30:03 +0900

    feat: swagger test - api tags and operation swagger 모듈 테스트를 위해 현재 구현된 로그인 API에 설명을 추가했습니다.

--- srcs/nest/src/auth/auth.controller.ts
+++ srcs/nest/src/auth/auth.controller.ts
@@ -3,14 +3,20 @@ import { AuthService } from './auth.service';
 import { FortytwoAuthGuard } from './fortytwo.guard';
 import { Auth42Dto } from './dto/auth42.dto';
 import { UserService } from 'src/user/user.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('authorization')
 export class AuthController {
     constructor(
         private authService: AuthService,
         private userService: UserService,
     ) {}
 
+    @ApiOperation({
+        summary: '42 login API',
+        description: '인트라 로그인 페이지로 리다이렉트 합니다.',
+    })
     @Get('/42login')
     @UseGuards(FortytwoAuthGuard)
     async login() {
@@ -19,6 +25,11 @@ export class AuthController {
         return 'success';
     }
 
+    @ApiOperation({
+        summary: '42 login callback API',
+        description:
+            '인트라 로그인 후, 신규 유저인지 기존 유저인지 판별합니다.',
+    })
     @Get('/callback')
     @UseGuards(FortytwoAuthGuard)
     async callBack() {
