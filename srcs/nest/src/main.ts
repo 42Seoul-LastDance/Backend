21f964c7cad319f27ef741cd0e135655f840de22
HyunJun KIM <hihj070914@icloud.com>
2023-09-06 20:54:04 +0900

    feat: configure nest swagger API 명세를 문서화하기 위한 swagger 초기 설정

--- srcs/nest/src/main.ts
+++ srcs/nest/src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-
     app.enableCors();
 
+    const config = new DocumentBuilder()
+        .setTitle('ft_transcendence API')
+        .setDescription('API description for ft_transcendence')
+        .setVersion('1.0.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    // Swagger UI에 대한 path를 연결함
+    // .setup('swagger ui endpoint', app, swagger_document)
+    SwaggerModule.setup('api', app, document);
     await app.listen(3000);
 }
 bootstrap();
