1f8c48f1f82fd823ac8584c17e9a4b392e37c830
JaeJunday <wowns8114@gmail.com>
2023-11-03 21:01:32 +0900

    chore: pakage-lock react-native 삭제
    - 스타일을 위한 react-native는 불필요한 번들크기 증가로 이어져서
      pakage의 크기를 줄이고자 의존성에서 삭제

--- services/next/init.sh
+++ services/next/init.sh
@@ -3,13 +3,13 @@ echo "----- Start installing npm dependencies -----"
 npm install --prefix srcs
 npm cache clean --force
 
-npm install --prefix srcs --save redux react-redux @reduxjs/toolkit react-devtools react-router-dom react-cookie jsonwebtoken
-npm install --prefix srcs --save react-unity-webgl nextjs-unity-webgl
-npm install --prefix srcs --save axios axios-auth-refresh
-npm install --prefix srcs --save ws socket.io utf-8-validate bufferutil
-npm install --prefix srcs --save @typescript-eslint
-npm install --prefix srcs --save babel-loader @babel/preset-react @babel/preset-env ts-loader
-npm install --prefix srcs --save @mui/material @mui/styles @emotion/react @emotion/styled @fontsource/roboto @mui/icons-material
+# npm install --prefix srcs --save redux react-redux @reduxjs/toolkit react-devtools react-router-dom react-cookie jsonwebtoken
+# npm install --prefix srcs --save react-unity-webgl nextjs-unity-webgl
+# npm install --prefix srcs --save axios axios-auth-refresh
+# npm install --prefix srcs --save ws socket.io utf-8-validate bufferutil
+# npm install --prefix srcs --save @typescript-eslint
+# npm install --prefix srcs --save babel-loader @babel/preset-react @babel/preset-env ts-loader
+# npm install --prefix srcs --save @mui/material @mui/styles @emotion/react @emotion/styled @fontsource/roboto @mui/icons-material
 
 echo "----- 😎 Successfully installed -----"
 
